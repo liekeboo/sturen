acf6e12fb118c54ba14399d57169679a4b2ca392
liekeboo <160623259+liekeboo@users.noreply.github.com>
2024-04-23 15:41:16 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,38 @@
+basic.forever(function () {
+    if (input.acceleration(Dimension.Y) < -1000) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (input.acceleration(Dimension.Y) > 1000) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (input.acceleration(Dimension.X) < -1000) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (input.acceleration(Dimension.X) > 1000) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
